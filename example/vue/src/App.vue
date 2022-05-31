d2d9883320cb85afebdb403fc8e4ccad50bd5786
xiaoxunyao <huchengyea@163.com>
2022-05-31 14:45:36 +0800

    fix: update h2 content

--- example/vue/src/App.vue
+++ example/vue/src/App.vue
@@ -5,7 +5,7 @@ import VueMultiColorText from '@elonehoo/vue-multi-color-text'
 <template>
   <main class="font-sans px-4 py-10 text-center gray-700 dark:gray-200">
     <VueMultiColorText>
-      <h2>abcdefghijkdddddddddddddabcdefghijkdddddddddddddabcdefghijkdddddddddddddabcdefghijkddddddddddddd</h2>
+      <h2>multi color text</h2>
     </VueMultiColorText>
   </main>
 </template>
