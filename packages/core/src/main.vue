a1092bbfe5667e8d45b9d69662b003bc9d79c6ed
xiaoxunyao <huchengyea@163.com>
2022-05-31 14:44:49 +0800

    fix: add props

--- packages/core/src/main.vue
+++ packages/core/src/main.vue
@@ -1,62 +1,63 @@
 <script setup lang="ts">
-const props = withDefaults(defineProps<{
+  const props = withDefaults(defineProps<{
     color1?:string,
     color2?:string,
     color3?:string,
     color4?:string,
     color5?:string,
+    time?:number,
+    angle?:number
   }>(),{
     color1:'#01579B',
     color2:'#00838F',
     color3:'#FFD600',
     color4:'#FF4081',
-    color5:'#F44336'
+    color5:'#F44336',
+    time:4,
+    angle:225
   })
+
+  let times:string = props.time + 's'
+  let angle:string = props.angle + 'deg'
+
 </script>
 
 <template>
-    <div :class="classes.multiColor">
+    <div class="multiColor">
       <slot/>
     </div>
 </template>
 
+<style scoped>
 
-<style module="classes">
-:root{
-  --color1: v-bind('props.color1');
-  --color2: v-bind('props.color2');
-  --color3: v-bind('props.color3');
-  --color4: v-bind('props.color4');
-  --color5: v-bind('props.color5');
-}
 .multiColor {
-  background:linear-gradient(225deg,
-    var(--color1) 0%, var(--color1) 9%,
+  background:linear-gradient(v-bind('angle'),
+    v-bind('props.color1') 0%, v-bind('props.color1') 9%,
     transparent 9%, transparent 10%,
-    var(--color2) 10%, var(--color2) 19%,
+    v-bind('props.color2') 10%, v-bind('props.color2') 19%,
     transparent 19%, transparent 20%,
-    var(--color3) 20%, var(--color3) 29%,
+    v-bind('props.color3') 20%, v-bind('props.color3') 29%,
     transparent 29%, transparent 30%,
-    var(--color4) 30%, var(--color4) 39%,
+    v-bind('props.color4') 30%, v-bind('props.color4') 39%,
     transparent 39%, transparent 40%,
-    var(--color5) 40%, var(--color5) 49%,
+    v-bind('props.color5') 40%, v-bind('props.color5') 49%,
     transparent 49%, transparent 50%,
-    var(--color1) 50%, var(--color1) 59%,
+    v-bind('props.color1')  50%, v-bind('props.color1')  59%,
     transparent 59%, transparent 60%,
-    var(--color2) 60%, var(--color2) 69%,
+    v-bind('props.color2') 60%, v-bind('props.color2') 69%,
     transparent 69%, transparent 70%,
-    var(--color3) 70%, var(--color3) 79%,
+    v-bind('props.color3') 70%, v-bind('props.color3') 79%,
     transparent 79%, transparent 80%,
-    var(--color4) 80%, var(--color4) 89%,
+    v-bind('props.color4') 80%, v-bind('props.color4') 89%,
     transparent 89%, transparent 90%,
-    var(--color5) 90%, var(--color5) 99%,
+    v-bind('props.color5') 90%, v-bind('props.color5') 99%,
     transparent 99%
   );
   background-clip: text;
   -webkit-background-clip: text;
   background-size: 200% 200%;
   color: transparent;
-  animation: move 4s linear infinite;
+  animation: move v-bind('times') linear infinite;
 }
 @keyframes move {
   0%{
