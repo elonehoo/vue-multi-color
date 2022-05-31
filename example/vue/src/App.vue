571d00c6ced7d3a96fdc6d132cbf48995cd8934f
xiaoxunyao <huchengyea@163.com>
2022-05-31 14:25:04 +0800

    chore: init

---
+++ example/vue/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import VueMultiColorText from '@elonehoo/vue-multi-color-text'
+</script>
+
+<template>
+  <main class="font-sans px-4 py-10 text-center gray-700 dark:gray-200">
+    <VueMultiColorText>
+      <h2>abcdefghijkdddddddddddddabcdefghijkdddddddddddddabcdefghijkdddddddddddddabcdefghijkddddddddddddd</h2>
+    </VueMultiColorText>
+  </main>
+</template>
+
+<style>
+</style>
